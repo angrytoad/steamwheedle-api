316fd5283e7b6cc9a2745efb9cacf41faff7051e
Tom Freeborough <tom.freeborough@thrivelearning.com>
2019-01-13 22:45:13 +0000

    [CORS] Add origin

--- config/cors.php
+++ config/cors.php
@@ -18,6 +18,7 @@ return [
         '192.168.99.100',
         'https://steamwheedle.com',
         'http://steamwheedle.com.s3-website.eu-west-2.amazonaws.com',
+        'https://play.steamwheedle.com'
     ],
     'allowedOriginsPatterns' => [],
     'allowedHeaders' => ['*'],
