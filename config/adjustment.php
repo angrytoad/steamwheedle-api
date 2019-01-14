083528158d8aceb6898dcb21fd54939e110d5998
Tom Freeborough <tom.freeborough@thrivelearning.com>
2019-01-14 00:31:18 +0000

    [INTERVAL] Change interval to use .env settings

--- config/adjustment.php
+++ config/adjustment.php
@@ -2,7 +2,7 @@
     // -1 round down, 0 normal rounding, 1 round up
     'rounding' => 1,
     // The interval period in seconds currently expressed in a readable way 5 * 60 seconds or 5 min
-    'interval' => 5 * 60,
+    'interval' => env('INTERVAL') * 60,
     // The multiple of an items base price that is considered the maximum price
     'upperBound' => 2.5,
     // The multiple of an items base price that is considered the minimum price
