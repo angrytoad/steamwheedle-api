d777b499e20ea0b73abc7cba53ee4a8678f16024
Tom Freeborough <tom.freeborough@thrivelearning.com>
2019-01-14 16:40:48 +0000

    [Adjustment] Wombo combo mode

--- config/adjustment.php
+++ config/adjustment.php
@@ -4,7 +4,7 @@
     // The interval period in seconds currently expressed in a readable way 5 * 60 seconds or 5 min
     'interval' => env('INTERVAL') * 60,
     // The multiple of an items base price that is considered the maximum price
-    'upperBound' => 2.5,
+    'upperBound' => 5,
     // The multiple of an items base price that is considered the minimum price
-    'lowerBound' => 1 / 2.5,
+    'lowerBound' => 1 / 5,
     ];
