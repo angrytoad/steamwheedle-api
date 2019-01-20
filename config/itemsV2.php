bf46579d45cbf9a607d7437be04fcbea3082cc27
Tom Freeborough <tom.freeborough@thrivelearning.com>
2019-01-20 02:03:06 +0000

    [Items V2] Add example v2 importer
    I've thought of a better way we can import items into the game by using the classicdb.ch website to pull all the data in for us, the only thing we need to do is add a description and the url of the item.
    
    Risk should be randomly assigned with there being more lower risk items in the game.

---
+++ config/itemsV2.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * DEV NOTE:
+ * USE THIS URL FOR FETCHING IMAGES
+ *
+ * http://classicdb.ch/images/icons/large/
+ *
+ *
+ * RISK WILL BE RANDOMLY ASSIGNED AS FOLLOWS
+ *
+ * 30% VERY LOW
+ * 25% LOW
+ * 20% MEDIUM
+ * 15% HIGH
+ * 10% VERY HIGH
+ */
+
+/**
+ * Item Categories
+
+    'Weapons',
+    'Armor',
+    'Container',
+    'Consumable',
+    'Reagents',
+    'Miscellaneous',
+    'Alchemy',
+    'Blacksmithing',
+    'Enchanting',
+    'Engineering',
+    'Herbalism',
+    'Leatherworking',
+    'Mining',
+    'Skinning',
+    'Tailoring'
+
+ */
+return [
+    'Weapons' => [
+        'Darkwood Fishing Pole' => [
+            'description' => 'Don\'t leave this in the water too long or it\'ll rot.',
+            'url' => 'http://classicdb.ch/?item=6366'
+        ],
+        'Antenna of Invigoration' => [
+            'description' => 'Its twitching but it makes me feel ALIVE!',
+            'url' => 'http://classicdb.ch/?item=21801'
+        ],
+        'Big Iron Fishing Pole' => [
+            'description' => 'The bigger the pole the bigger the fish',
+            'url' => 'http://classicdb.ch/?item=6367'
+        ],
+        'Starfaller' => [
+            'description' => 'This wand tingles with cosmic energy.',
+            'url' => 'http://classicdb.ch/?item=13063'
+        ],
+        'Freezing Shard' => [
+            'description' => 'Just holding this chills your bones.',
+            'url' => 'http://classicdb.ch/?item=10572'
+        ],
+        'Torch of Austen' => [
+            'description' => 'This torch seems to burn forever',
+            'url' => 'http://classicdb.ch/?item=13004'
+        ],
+        'Jaina\'s Firestarter' => [
+            'description' => 'Relight my fire.',
+            'url' => 'http://classicdb.ch/?item=13064'
+        ],
+    ]
+];
